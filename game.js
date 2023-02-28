778423792291f664f4a9a5ede2cab4ef6a50a736
Indukuri Likhitha <likhithaindukuri07@gmail.com>
2023-02-28 22:34:59 +0530

    Modified

--- game.js
+++ game.js
@@ -24,3 +24,25 @@ $(".btn").click(function() {
 
   checkAnswer(userClickedPattern.length-1);
 });
+
+
+function checkAnswer(currentLevel) {
+
+    if (gamePattern[currentLevel] === userClickedPattern[currentLevel]) {
+      if (userClickedPattern.length === gamePattern.length){
+        setTimeout(function () {
+          nextSequence();
+        }, 1000);
+      }
+    } else {
+      playSound("wrong");
+      $("body").addClass("game-over");
+      $("#level-title").text("Game Over, Press Any Key to Restart");
+
+      setTimeout(function () {
+        $("body").removeClass("game-over");
+      }, 200);
+
+      startOver();
+    }
+}
