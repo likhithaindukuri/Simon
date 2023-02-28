9a402e08cc9802e9fd79adcea6a6683973f565f5
Indukuri Likhitha <likhithaindukuri07@gmail.com>
2023-02-28 22:33:31 +0530

    Modified

---
+++ game.js
@@ -0,0 +1,15 @@
+var buttonColours = ["red", "blue", "green", "yellow"];
+
+var gamePattern = [];
+var userClickedPattern = [];
+
+var started = false;
+var level = 0;
+
+$(document).keypress(function() {
+  if (!started) {
+    $("#level-title").text("Level " + level);
+    nextSequence();
+    started = true;
+  }
+});
