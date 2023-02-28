f3e84eb168451f39c144b1450f2e6d59b209152e
Indukuri Likhitha <likhithaindukuri07@gmail.com>
2023-02-28 22:35:42 +0530

    Modified

--- game.js
+++ game.js
@@ -46,3 +46,16 @@ function checkAnswer(currentLevel) {
       startOver();
     }
 }
+
+
+function nextSequence() {
+  userClickedPattern = [];
+  level++;
+  $("#level-title").text("Level " + level);
+  var randomNumber = Math.floor(Math.random() * 4);
+  var randomChosenColour = buttonColours[randomNumber];
+  gamePattern.push(randomChosenColour);
+
+  $("#" + randomChosenColour).fadeIn(100).fadeOut(100).fadeIn(100);
+  playSound(randomChosenColour);
+}
